bfa9f2232d9368063360b61801706fbc92713054
nagura4000 <nagura4000@gmail.com>
2015-12-21 09:02:42 +0900

    modified

---
+++ app/src/main/java/com/arugan/immunity/DailyScheduler.java
@@ -0,0 +1,7 @@
+package com.arugan.immunity;
+
+/**
+ * Created by nagura on 2015/12/09.
+ */
+public class DailyScheduler {
+}
---
+++ app/src/main/java/com/arugan/immunity/dao/MySQLiteOpenHelper.java
@@ -0,0 +1,7 @@
+package com.arugan.immunity.dao;
+
+/**
+ * Created by nagura on 2015/12/08.
+ */
+public class MySQLiteOpenHelper {
+}
